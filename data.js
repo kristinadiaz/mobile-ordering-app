d31e6d1e6156f38e45d731f20f14a542b862cebb
Kristina Diaz <kristina.diaz84@gmail.com>
2023-01-05 16:08:58 -0700

    added data file

---
+++ data.js
@@ -0,0 +1,26 @@
+const bookArray = [
+    {
+        title: "Sea of Tranquility",
+        author: 'Emily St. John Mandel',
+        genres: ["fiction", "sci-fi", "drama"],
+        id: 0,
+        price: 14,
+        emoji: "📚"
+    },
+    {
+        title: "A Visit from the Goon Squad",
+        author: 'Jennifer Egan',
+        genres: ["punk", "music", "fiction"],
+        id: 1,
+        price: 12,
+        emoji: "📖"
+    },
+        {
+        title: "A Promised Land",
+        author: 'Barack Obama',
+        genres: ["autobiography, political, events, president"],
+        id: 2,
+        price: 12,
+        emoji: "📕"
+    }
+]
